81117063fcf5ac7ec34295938fae1fbaca934c0c
DR02 <02diegorf@gmail.com>
2025-02-18 23:42:25 +0100

    Update files

--- data/data.js
+++ data/data.js
@@ -1,5 +1,9 @@
 export const data = {
     nombre: "Juan Pérez",
     fecha: new Date().toLocaleDateString(),
-    reporte: "Este es el informe del mes de febrero."
+    reporte: "Este es el informe del mes de febrero.",
+    precio:{
+        neto:"10",
+        bruto:"9.99"
+    }
 };
